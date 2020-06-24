e8641009b74f51b794830b1bfabbb41fbb5f1661
shadman-a <shadman@outlook.com>
2020-06-24 14:57:58 -0400

    poop

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+get '/artists', to: 'artists#index', as: 'artists'
+
+get '/artists/new', to: 'artists#new', as: 'new_artist'
+post '/artists', to: 'artists#create'
+
+get '/artists/:id', to: 'artists#show', as: 'artist'
 end
